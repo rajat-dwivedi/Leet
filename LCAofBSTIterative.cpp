2c7132797b4e908e8fcc97e252bdaea3d5a5e813
RjD402 <rdroid302@gmail.com>
2021-03-10 22:04:23 +0530

    problem

---
+++ LCAofBSTIterative.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* cur = root;
+        while (true) {
+            if (p -> val < cur -> val && q -> val < cur -> val) {
+                cur = cur -> left;
+            } else if (p -> val > cur -> val && q -> val > cur -> val) {
+                cur = cur -> right;
+            } else {
+                break;
+            }
+        }
+        return cur;
+    }
+};
