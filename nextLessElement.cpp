4258f3ddc9f98372a8a93e3ac7b214a3802afe3f
RjD402 <rdroid302@gmail.com>
2021-03-14 21:55:14 +0530

    nextLessElement

---
+++ nextLessElement.cpp
@@ -0,0 +1,10 @@
+// next_less[i] = j means A[j] is the next less element of A[i].
+// next_less[i] = -1 means there is no next less element of A[i].
+vector<int> previous_less(A.size(), -1);
+for(int i = 0; i < A.size(); i++){
+  while(!in_stk.empty() && A[in_stk.top()] > A[i]){
+    auto x = in_stk.top(); in_stk.pop();
+    next_less[x] = i;
+  }
+  in_stk.push(i);
+}
