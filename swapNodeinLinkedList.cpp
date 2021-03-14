10f526047786af5cfa4ca960aa9a483390c66aef
RjD402 <rdroid302@gmail.com>
2021-03-14 16:26:12 +0530

    swapNodesinLinkedList.cpp

--- swapNodeinLinkedList.cpp
+++ swapNodeinLinkedList.cpp
@@ -12,9 +12,9 @@ class Solution {
 public:
     // https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
     ListNode* swapNodes(ListNode* head, int k) {
-    ListNode *n1 = nullptr, *n2 = nullptr;
-    for (auto p = head; p != nullptr; p = p->next) {
-        n2 = n2 == nullptr ? nullptr : n2->next;
+    ListNode *n1 = NULL, *n2 = NULL;
+    for (auto p = head; p != NULL; p = p->next) {
+        n2 = n2 == NULL ? NULL : n2->next;
         if (--k == 0) {
             n1 = p;
             n2 = head;
