d17777a748ad4cdee62f9c7735a89bbc656ed47d
RjD402 <rdroid302@gmail.com>
2021-03-16 00:15:59 +0530

    sortedArrayToBST.cpp

---
+++ sortedArrayToBST.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+class Solution {
+    TreeNode* sortedArrayToBST(vector<int>& nums, int start, int end){
+        if(end<=start) return NULL; 
+        int midIdx=(end+start)/2;
+        TreeNode* root=new TreeNode(nums[midIdx]);
+        root->left=sortedArrayToBST(nums, start, midIdx);
+        root->right=sortedArrayToBST(nums, midIdx+1,end);
+        return root;
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBST(nums, 0,nums.size());
+    }
+};
