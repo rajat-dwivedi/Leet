dcb64597ab9b8bf179802949fdde7ae9632c6681
RjD402 <rdroid302@gmail.com>
2021-04-03 13:40:32 +0530

    longestValidParenthesis.cpp

---
+++ longestValidParenthesis.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-valid-parentheses/
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int maxL=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int t=stk.top();
+            if(t!=-1&&s[i]==')'&&s[t]=='(')
+            {
+                stk.pop();
+                maxL=max(maxL,i-stk.top());
+            }
+            else
+                stk.push(i);
+        }
+        return maxL;
+    }
+};
