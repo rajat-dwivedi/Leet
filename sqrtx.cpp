bfd5b12fc2112a949c4e762291b845bd0e1835d3
RjD402 <rdroid302@gmail.com>
2021-04-04 01:00:33 +0530

    sqrtx.cpp

---
+++ sqrtx.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/sqrtx/
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0|| x==1)
+            return x;
+        int start =1;
+        int end = x;
+        int ans;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            if(mid==x/mid)
+                return mid;
+            if(mid<x/mid)
+            {
+                start = mid+1;
+                ans = mid;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+};
