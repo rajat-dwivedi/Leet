190996df1455edffc128783798c9465a8fd26ebe
RjD402 <rdroid302@gmail.com>
2021-04-04 17:06:19 +0530

    findkthlargetnaive

--- findKthLargestNaive.cpp
+++ findKthLargestNaive.cpp
@@ -1,4 +1,6 @@
 //https://leetcode.com/problems/kth-largest-element-in-an-array/
+//naive solution. Just sort the array in reverse order and return the kth largest element
+//the time complexity of this solution is O(NlogN)
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
