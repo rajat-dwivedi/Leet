69ba1e98d5af3b4c586ce004726d7c5e27dec619
RjD402 <rdroid302@gmail.com>
2021-04-04 13:44:09 +0530

    removeDuplicatesFromSortedArray2.cpp

---
+++ removeDuplicatesFromSortedArray2.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i =0;
+        for(auto& num: nums){
+            if(i<2 || num > nums[i-2]){
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+};
