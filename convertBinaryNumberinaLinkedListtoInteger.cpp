e4ee86f59cac3c1085e42e373908331beeb30e8f
RjD402 <rdroid302@gmail.com>
2021-04-10 00:31:23 +0530

    convertBinaryNumbersinaLinkedList.cpp

---
+++ convertBinaryNumberinaLinkedListtoInteger.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+int getDecimalValue(ListNode* head) {
+        int num = 0;
+        ListNode* curr_node = head;
+        while( curr_node != nullptr ) {
+            num <<= 1;
+            num |= curr_node->val;
+            curr_node = curr_node->next;
+        }
+        //Method2
+        // int ans=0;
+        // while(head)
+        // {
+        //     ans=ans*2 + (head->val);
+        //     head=head->next;
+        // }
+        // return ans;
+        return num;
+    }
+};
