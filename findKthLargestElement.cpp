b07a1ca46bbedd226a4d693f365b73debcd41339
RjD402 <rdroid302@gmail.com>
2021-04-16 16:41:56 +0530

    findKthLargestElement.cpp

--- findKthLargestElement.cpp
+++ findKthLargestElement.cpp
@@ -1,8 +1,10 @@
+
 //https://leetcode.com/problems/kth-largest-element-in-an-array/
+//naive solution is to sort in decreasing order and print the kth element from the beginning - O(nlogn)+O(1)memory
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        //heap solution with time complexity of O(nlogk) 
+        //heap solution with time complexity of O(nlogk)TC + O(k)memory
         //when asked the largest then create a min heap as min element will rise to top and will be poped
         priority_queue<int, vector<int>,greater<int>> pq;
         for(auto& num:nums){
