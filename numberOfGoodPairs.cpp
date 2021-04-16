116bddb8694d1b47a1bf65daa0c869b6ff2cd7e0
RjD402 <rdroid302@gmail.com>
2021-04-17 00:55:50 +0530

    numberofGoodPairs.cpp

---
+++ numberOfGoodPairs.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/number-of-good-pairs/
+class Solution {
+public:
+        int numIdenticalPairs(vector<int>& A) {
+        int res = 0;
+        unordered_map<int, int> count;
+        for (int a: A) {
+            res += count[a]++;
+        }
+     
+        return res;
+    }
+};
