cee2bfb120f0aa01d016efe049f4c429f6348df0
RjD402 <rdroid302@gmail.com>
2021-04-20 22:10:39 +0530

    validParenthesis.cpp

---
+++ validParenthesis.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/valid-parentheses/
+class Solution {
+public:
+    bool isValid(string s) {
+    stack<char> st;
+    for(char c : s){
+        if(c == '('|| c == '{' || c == '['){
+            st.push(c);
+        }else{
+            if(st.empty()) return false;
+            if(c == ')' && st.top() != '(') return false;
+            if(c == '}' && st.top() != '{') return false;
+            if(c == ']' && st.top() != '[') return false;
+            st.pop();
+        }
+    }
+    return st.empty();
+    }
+};
