dd09a52d905c56d92d1c2c97c62efbde97308894
Rajat Dwivedi <rdroid302@gmail.com>
2021-05-24 23:49:23 +0530

    subarraySumEqualsK.cpp

--- subarraySumEqualsK.cpp
+++ subarraySumEqualsK.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int cum=0; // cumulated sum
-        map<int,int> rec; // prefix sum recorder
+        unordered_map<int,int> rec; // prefix sum recorder
         int cnt = 0; // number of found subarray
         rec[0]++; // to take into account those subarrays that begin with index 0
         for(int i=0;i<nums.size();i++){
