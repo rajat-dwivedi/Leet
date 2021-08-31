d789405d09c82e4109bfcaade1044650722aaaa5
RjD402 <rdroid302@gmail.com>
2021-08-31 16:06:00 -0500

    twoSum.java

---
+++ twoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> mp = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(mp.containsKey(target-nums[i])){
+                res[0] = i;
+                res[1] = mp.get(target-nums[i]);
+                break;
+            }else{
+                mp.put(nums[i],i);
+            }
+        }
+        return res;
+    }
+}
