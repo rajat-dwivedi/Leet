7b6f599fcbed3c887d56015775297e586ad0dd44
RjD402 <rdroid302@gmail.com>
2021-09-05 17:25:30 -0500

    maxChunksToMakeSorted2.java

---
+++ maxChunksToMakeSorted2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        
+        int[] rmin = new int[n+1];
+        rmin[n] = Integer.MAX_VALUE;
+        
+        int count = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            rmin[i] = Math.min(rmin[i+1],arr[i]);
+        }
+        
+        int lmax = Integer.MIN_VALUE;
+        
+        for(int i=0;i<n;i++){
+            lmax = Math.max(lmax, arr[i]);
+            if(lmax<=rmin[i+1]){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
