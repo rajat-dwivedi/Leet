5c04bcb9df986d159897e87fdd73ccfac9d2b203
RjD402 <rdroid302@gmail.com>
2021-09-11 21:04:51 -0500

    validPalindrome.cpp

---
+++ validPalindrome.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+     for(int i=0,j=s.length()-1;i<j;i++,j--){
+         while(!isalnum(s[i]) && i<j) i++;
+         while(!isalnum(s[j]) && j>i) j--;
+         if(toupper(s[i])!=toupper(s[j]))
+             return false;
+     }   
+        return true;
+    }
+};
