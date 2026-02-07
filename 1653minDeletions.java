21d625f14837a3dc64278d961e8430f31ef2b4a4
rajat-dwivedi <rdroid302@gmail.com>
2026-02-06 20:40:17 -0600

    min deletions

---
+++ 1653minDeletions.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/
+class Solution {
+    public int minimumDeletions(String s) {
+        int res = 0, count = 0;
+        for (char c : s.toCharArray()){
+            if (c == 'b')
+                count++;
+            else if (count != 0){
+                res++;
+                count--;
+            }
+        }
+        return res;
+    }
+}
